1ae66901538490891fa450dcdd5c5e4f5127ea5f
DavidCarlDanelle <2024-200388@rtu.edu.ph>
2026-09-13 23:25:30 +0800

    last_words

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const message = document.getElementById("message");
 const button = document.getElementById("changeButton");
 
 button.addEventListener("click", () => {
-    message.textContent = "You just changed the project!";
+    message.textContent = "Rene: MAMAAAA!";
 });
